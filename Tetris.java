1307ce025ab8c95cd88ec95255025f4d3fa48098
Prabhat Kadam <prabhatkadam18@gmail.com>
2019-07-23 02:00:00 +0530

    Tetris Game (player does not lose yet)
    No Undo/Redo
    Player does not lose

--- Tetris.java
+++ Tetris.java
@@ -3,7 +3,7 @@
 import java.util.*;
 
 class Board{
-  public int row = 22,col = 22;
+  public int row = 20,col = 20;
   public char ar[][] = new char [row][col];
   Board(){
     for(int i =0;i < row ;i++){
@@ -118,6 +118,14 @@ class Point{
 
 class Shape{
   public Point version[][];
+  // public int currentShape;
+  // public Point currentLocation;
+  // public int ver;
+  // public Shape(int s, Point l, int v){
+  //   currentShape = s;
+  //   currentLocation = l;
+  //   ver = v;
+  // }
   public Shape(){
   }
   public Shape(Shape shape){
@@ -129,7 +137,19 @@ class Shape{
   }
 }
 
-public class Tetris{
+// interface ActionStack {
+//   public class ShapeObject{
+//     Point location;
+//     int shape;
+//     int version;
+//   }
+//   char actions[] = new char[40];
+
+//   public void push(char action);
+//   public int pop();
+// }
+
+public class Tetris {
 
 
   public static void clearScreen() {
@@ -137,9 +157,12 @@ public class Tetris{
     System.out.flush();
   }
 
+  // public void push(char action){
 
+  // }
 
   public static void main(String args[]){
+ 
 
     Shape shapes[] = new Shape[5];
 
@@ -148,7 +171,8 @@ public class Tetris{
     }
     
 
-    ///////////////////////
+    ///////////////////////   Shapes Points
+    {
     shapes[0].version = new Point[2][4];
     shapes[0].version[0][0] = new Point(0, 0);
     shapes[0].version[0][1] = new Point(-1, 0);
@@ -156,9 +180,9 @@ public class Tetris{
     shapes[0].version[0][3] = new Point(-3, 0);
 
     shapes[0].version[1][0] = new Point(0, 0);
-    shapes[0].version[1][1] = new Point(0, 1);
-    shapes[0].version[1][2] = new Point(0, 2);
-    shapes[0].version[1][3] = new Point(0, 3);
+    shapes[0].version[1][1] = new Point(0, -1);
+    shapes[0].version[1][2] = new Point(0, 1);
+    shapes[0].version[1][3] = new Point(0, 2);
 
     shapes[1].version = new Point[4][4];
     shapes[1].version[0][1] = new Point(0, 0); // L
@@ -218,27 +242,37 @@ public class Tetris{
     shapes[4].version[3][1] = new Point(0, 1);
     shapes[4].version[3][2] = new Point(-1, 1);
     shapes[4].version[3][3] = new Point(0, 0);
+    }
 
     //////////////////////
 
     Random rand = new Random();
     Board board = new Board();
     Scanner sc = new Scanner(System.in);
-    Point currentLocation = new Point(board.row - 3, rand.nextInt(board.col - 5) + 2);
-    
 
+
+    Point currentLocation = new Point(board.row - 3, rand.nextInt(board.col - 6) + 2);
     int randomShape = rand.nextInt(5);
     int randomVersion = rand.nextInt(shapes[randomShape].version.length);
 
+    // Shape obj = new Shape(randomShape,currentLocation,randomVersion);
+    // undo.push(obj);
 
+    // Stack undo = new Stack();
+    // Stack redo = new Stack();
     
     Shape currentShape = new Shape();
     currentShape = shapes[randomShape];
     
     char ch = '0';
+    int set = 0;
     boolean running = true;
     int ver  = randomVersion;
     while( running == true){
+      if(ch == 'q'){
+        running = false;
+        break;
+      }
       if(ch  == 'l')
       {
         //ch = 's';
@@ -246,7 +280,7 @@ public class Tetris{
           board.clearBoardOnRotate(currentShape, currentLocation, ver);
         }
         ver = ver <= 0?currentShape.version.length-1:(ver - 1);
-
+        
       }
       else if(ch == 'k'){
         // ch = 's';
@@ -254,9 +288,15 @@ public class Tetris{
           board.clearBoardOnRotate(currentShape, currentLocation, ver);
         }
         ver = (ver+1)%currentShape.version.length;
+        
       }
+
       if (ch == 'd') {
         board.moveRight(currentShape, ver, currentLocation);
+        ch = 's';
+      }
+      else if(ch == 'w'){
+        board.moveUp(currentShape, ver, currentLocation);
         ch = 's';
       }
       else if(ch == 'a'){
@@ -266,6 +306,7 @@ public class Tetris{
       else if(ch == 's'){
         int canMove = board.moveDown(currentShape, ver, currentLocation);
         if(canMove == 0){
+          set = 1;
           randomShape = rand.nextInt(5);
           randomVersion = rand.nextInt(shapes[randomShape].version.length);
           ver = randomVersion;
@@ -273,9 +314,17 @@ public class Tetris{
           currentShape = shapes[randomShape];
         }
       }
-      
+      // if(ch == 'z'){
+
+      // }
+      // obj = new Shape(randomShape,currentLocation,ver);
+      // undo.push(obj);
+
       board.draw(currentShape, currentLocation, ver);
-      board.deleteRow();
+      if(set == 1){
+        board.deleteRow();
+        set = 0;
+      }
       board.printBoard();
 
       
